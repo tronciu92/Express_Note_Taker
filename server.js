7e77469927ebcd57161cd8a0e4f04bcd6b9ea3ac
Vasile <tronciu92@gmail.com>
2020-09-26 10:29:47 -0700

    server fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 const app = express();
 const port = 8080;
-const mainDir = path.join(__dirname, "/public");
+const mainDir = path.join(__dirname, "./public");
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
@@ -15,7 +15,7 @@ app.get("/notes", function(req, res) {
 });
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 app.get("/api/notes/:id", function(req, res) {
